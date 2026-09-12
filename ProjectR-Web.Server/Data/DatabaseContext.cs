a9414203945ec2a5de65534e0baf7467fb9f01fe
The-Nightman <keenan.brant@yahoo.co.uk>
2026-09-12 20:19:51 +0100

    add DatabaseContext and initial migration to test database creation with unfamiliar driver

---
+++ ProjectR-Web.Server/Data/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectR_Web.Server.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options) { }
+    }
+}
--- ProjectR-Web.Server/Program.cs
+++ ProjectR-Web.Server/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectR_Web.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseMySql(
+        connString
+            ?? throw new InvalidOperationException(
+                "Connection string 'DefaultConnection' not found."
+            ),
+        ServerVersion.AutoDetect(connString)
+    )
+);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
